aa6d17f2bd00274d2847437c3cb02c7ba6d63c0b
Александр Решетов <areshetov@diasoft.ru>
2021-08-31 21:49:20 +0300

    Корпоративная программа Spring Framework. Домашнее задание 12. Реализовать обработку доменной сущности через каналы Spring Integration. Исправил по МР.

--- Homework12/src/main/java/ru/otus/homework12/config/IntegrationFlowConfig.java
+++ Homework12/src/main/java/ru/otus/homework12/config/IntegrationFlowConfig.java
@@ -1,5 +1,6 @@
 package ru.otus.homework12.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.channel.PublishSubscribeChannel;
@@ -7,12 +8,25 @@ import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.MessageChannels;
+import ru.otus.homework12.service.IntegrationTestService;
+import ru.otus.homework12.service.UnitTestService;
 
 @Configuration
 @EnableIntegration
 public class IntegrationFlowConfig {
+    private final UnitTestService unitTestService;
+    private final IntegrationTestService integrationTestService;
+
     public static final String UNIT_CHANNEL = "unitChannel";
     public static final String UNIT_TEST_CHANNEL = "unitTestChannel";
+    public static final String UNIT_TEST_SERVICE_METHOD_NAME = "test";
+    public static final String INTEGRATION_TEST_SERVICE_METHOD_NAME = "test";
+
+    @Autowired
+    public IntegrationFlowConfig(UnitTestService unitTestService, IntegrationTestService integrationTestService) {
+        this.unitTestService = unitTestService;
+        this.integrationTestService = integrationTestService;
+    }
 
     @Bean
     public PublishSubscribeChannel unitChannel() {
@@ -26,9 +40,9 @@ public class IntegrationFlowConfig {
 
     @Bean
     public IntegrationFlow unitTestFlow() {
-        return IntegrationFlows.from(UNIT_CHANNEL)
+        return IntegrationFlows.from(unitChannel())
                 .split()
-                .handle("unitTestService", "test")
+                .handle(unitTestService, UNIT_TEST_SERVICE_METHOD_NAME)
                 .aggregate()
                 .channel(UNIT_TEST_CHANNEL)
                 .get();
@@ -36,8 +50,8 @@ public class IntegrationFlowConfig {
 
     @Bean
     public IntegrationFlow integrationTestFlow() {
-        return IntegrationFlows.from(UNIT_TEST_CHANNEL)
-                .handle("integrationTestService", "test")
+        return IntegrationFlows.from(unitTestChannel())
+                .handle(integrationTestService, INTEGRATION_TEST_SERVICE_METHOD_NAME)
                 .get();
     }
 }
