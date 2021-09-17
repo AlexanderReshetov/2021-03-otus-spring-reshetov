9ecd70835d0df7108677826dea0f660252ef3e81
Александр Решетов <areshetov@diasoft.ru>
2021-09-17 10:21:58 +0300

    Корпоративная программа Spring Framework. Проектная работа. Загрузка, парсинг и анализ данных из внешних систем на примере Blizzard Developer APIs. Добавил юнит-тест Dto.

---
+++ ProjectWork/project-work-main/src/test/java/ru/otus/main/dto/TrendItemDtoTest.java
@@ -0,0 +1,25 @@
+package ru.otus.main.dto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Dto тренда должен")
+public class TrendItemDtoTest {
+    @Test
+    @DisplayName("корректно создаться конструктором")
+    void shouldHaveCorrectConstructor() {
+        final LocalDateTime localDateTime = LocalDateTime.now();
+        final Long price = 4L;
+        final TrendItemDto trendItemDto = new TrendItemDto(localDateTime, price);
+
+        assertEquals(localDateTime,trendItemDto.getLocalDateTime());
+        assertEquals(price, trendItemDto.getPrice());
+    }
+}
